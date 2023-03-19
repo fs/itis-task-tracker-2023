c43fcce28e808ecf0d4a105a3ebbab27d0f86c90
Juliana Samoylenko <yuliana.samoilenko@flatstack.com>
2023-03-19 11:23:59 +0400

    add scroll button for default template

--- src/components/templates/DefaultTemplate/DefaultTemplate.jsx
+++ src/components/templates/DefaultTemplate/DefaultTemplate.jsx
@@ -1,7 +1,9 @@
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
 import Header from "../../molecules/Header";
 import Footer from "../../molecules/Footer";
+import Button from "../../atoms/Button";
 
 const MainContent = styled.main`
   display: flex;
@@ -10,13 +12,48 @@ const MainContent = styled.main`
   min-height: calc(100vh - 52px - 62px);
   flex-grow: 1;
   padding: 1rem;
+  overflow: auto; // add this to enable overflow
+`;
+
+const ButtonWrapper = styled.div`
+  margin: 2rem 0;
+  position: fixed;
+  top: 40px;
+  right: 20px;
 `;
 
 const DefaultTemplate = ({ children }) => {
+  const [showScrollButton, setShowScrollButton] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.pageYOffset > 0) {
+        setShowScrollButton(true);
+      } else {
+        setShowScrollButton(false);
+      }
+    };
+
+    document.addEventListener("scroll", handleScroll);
+
+    return () => {
+      document.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleScrollToTop = () => {
+    window.scroll({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <>
       <Header />
       <MainContent>{children}</MainContent>
+      {showScrollButton && (
+        <ButtonWrapper>
+          <Button label="Scroll to top" onClick={handleScrollToTop} color="black" />
+        </ButtonWrapper>
+      )}
       <Footer />
     </>
   );
